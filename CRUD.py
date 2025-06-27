d5649e049d85b24d1ca887f2520ed3a42093e297
Herik <herikkou@gmail.com>
2025-06-26 23:29:08 -0300

    Add files via upload

--- CRUD.py
+++ CRUD.py
@@ -1,4 +1,15 @@
-cadastro =[]
+import json;
+def CarregarDados():
+   try:
+        with open("CRUD.json","r") as arquivo:
+             return json.load(arquivo)
+   except FileNotFoundError:
+        return []
+
+def salvar_dados(lista):
+    with open("cadastros.json", "w") as arquivo:
+        json.dump(lista, arquivo, indent=4)       
+cadastro =CarregarDados()
 
 
 while True:
@@ -27,6 +38,7 @@ while True:
             
 
             cadastro.append((nome,idade,email))
+            salvar_dados(cadastro)
             print("Cadastrado com sucesso")
                 
         case 2:
@@ -38,7 +50,8 @@ while True:
             for buscar in cadastro:
                     if buscar[0] == nome:
                              print(buscar)
-
+                           
+                             salvar_dados(buscar)   
                  
             
         case 3:
@@ -49,6 +62,8 @@ while True:
               for i in cadastro:
                print(i)
              
+               salvar_dados(i)
+             
         case 4: 
             if not cadastro:
                  print("Nenhuma pesssoa cadastrada")
@@ -58,7 +73,10 @@ while True:
                 if buscar[0] == nome:
                      cadastro.remove(buscar)
                      print(cadastro)
+                    
+                     salvar_dados(buscar)
                      print("Pessoa Removida com Sucesso")
+
                 else:
                      print("Erro ao remover a pessoa")     
             
